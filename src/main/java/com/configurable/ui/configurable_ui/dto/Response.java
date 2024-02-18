fa9dd6a206dbb79a2cee3489a4fd51ec9767b42e
Vishal Pandey <vishalpandey10022000@gmail.com>
2024-02-18 22:25:46 +0530

    Add files via upload

---
+++ src/main/java/com/configurable/ui/configurable_ui/dto/Response.java
@@ -0,0 +1,15 @@
+package com.configurable.ui.configurable_ui.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+public class Response {
+    private String message;
+
+}
